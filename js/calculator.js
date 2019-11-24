eaf45654e8d0b66ec259a48c75e8057c27864fa0
Benjamin Rubin <bennyfrubin@gmail.com>
2019-11-23 16:00:41 -0800

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,24 +1,29 @@
+// The display of the calculator
 var $display = $('#display');
 
+// Retrieve an array of all the buttons on the page
 var btnArray = $('.btn');
 
 // sVal = Stored Value
 var $sVal = null;
 
 // dVal = Displayed Value
-var $dVal = 0;
+var $dVal = "0";
 
 // wOp = Waiting Operator
 var $wOp = null
 
-
+// If I click on one of the buttons
 btnArray.click(function(e) {
 
+
+    // Store the HTML of the button
     var $btn = $(this).html();
 
     switch ($btn) {
 
         case 'AC':
+
             // If the display is not disabled
             if (!$display.prop("disabled")) {
                 resetCalculator();
@@ -28,26 +33,36 @@ btnArray.click(function(e) {
         case 'ON/OFF':
             // If it is disabled
             if ($display.prop("disabled")) {
+
                 // Enable it
                 $display.prop("disabled", false);
+
                 // Display the value "0"
                 display("0");
             }
             // It is enabled
             else {
                 resetCalculator();
+
                 // Disable the display
                 $display.prop("disabled", true);
+
                 // Display "OFF"
                 display("OFF");
             }
             break;
 
         case '/':
+            // If the stored value is not null
             if ($sVal) {
+
+                // That means we can calculate
                 calculate();
             }
+            // Store the waiting operator
             $wOp = $btn;
+
+            // Set the stored value to the displayed value
             setSvalToDval()
             break;
 
@@ -76,8 +91,11 @@ btnArray.click(function(e) {
             break;
 
         case '=':
+
             // Calculate
             calculate();
+
+            // Set the stored value to null
             $sVal = null;
 
             break;
@@ -85,27 +103,31 @@ btnArray.click(function(e) {
         case '.':
 
             // If string doesn't contain a period
-            if (!$dVal.includes(".")) {
+            console.log($dVal);
+            if (!$dVal.toString().includes('.')) {
+
                 // append a period to the currently displayed number
                 $dVal += ".";
+
+                // Display the new value
+                display($dVal);
             }
             // Otherwise do nothing
 
             break;
 
-            //Otherwise will be a number
+            // If it won't be any of the above, then it will be a number
         default:
-            console.log('Should be a number! Equal to ' + $btn);
 
+            // If the display value is not equal to 0
             if ($dVal != "0") {
+
+                // Append the number to whatever is displayed
                 $dVal += $btn;
             } else {
                 $dVal = $btn;
             }
             display($dVal);
-
-            // append it to the display value
-            // update the display to show the new appended value  
     }
 
 
@@ -132,13 +154,15 @@ function calculate() {
     var leftSide = parseFloat($sVal);
     var rightSide = parseFloat($dVal);
 
+    console.log("Left side is " + leftSide)
+    console.log("Right side is " + rightSide)
 
     switch ($wOp) {
         case '/':
             if (leftSide != 0 && rightSide == 0) {
                 $dVal = "Not Possible";
             } else {
-                $dVal = leftSide / rightSide;
+                $dVal = (leftSide * 100) / (rightSide * 100);
             }
             break;
 
@@ -154,5 +178,6 @@ function calculate() {
             $dVal = leftSide + rightSide;
             break;
     }
+
     display($dVal);
 }
