659ca9b7f58e1c56c62f8c87e3afbf197a2bdfd1
Benjamin Floyd Rubin <bennyfrubin@gmail.com>
2020-06-09 09:32:55 -0700

    Updated

--- js/signature.js
+++ js/signature.js
@@ -2,45 +2,62 @@
 var formName = $("#formName");
 var formEmail = $("#formEmail");
 var formPhone = $("#formPhone");
-var formRole = $("#formRole"); 
+var formRole = $("#formRole");
 
 // Signature Variables
 var signatureName = $("#signatureName");
 var signatureEmail = $("#signatureEmail");
 var signatureTarget = $("#signatureTarget");
 var signaturePhone = $("#signaturePhone");
-var signatureRole = $("#signatureRole"); 
+var signaturePhoneTarget = $("#signaturePhoneTarget");
+var signatureRole = $("#signatureRole");
 
 
-formName.keyup(function() {
+formName.keyup(function () {
     signatureName.html(formName.val());
 });
 
-formEmail.keyup(function() {
-    
+formEmail.keyup(function () {
+
     signatureEmail.html(formEmail.val());
     var targetValue = "mailto:" + formEmail.val();
     signatureTarget.attr('href', targetValue);
 });
 
-formPhone.keyup(function() {
+formPhone.keyup(function () {
 
     var formattedPhone = formatPhoneNumber(formPhone.val());
-
+    var targetValue = "tel:" + formPhone.val();
     signaturePhone.html(formattedPhone);
+    signaturePhoneTarget.attr('href', targetValue);
 })
 
 
-formRole.change(function() {
+// Change the Form Role
+formRole.change(function () {
     signatureRole.html(formRole.val())
 })
 
 
 
+// Copy Rich Text Function
 function copySignature() {
 
-    var signatureCode = document.getElementById("signature");
+    
+    // Step 1: Extract the HTLM code
+    var signatureCode = document.getElementById("signature").innerHTML;
 
+
+    function listener(e) {
+        e.clipboardData.setData("text/html", signatureCode);
+        // e.clipboardData.setData("text/plain", signatureCode);
+        e.preventDefault();
+    }
+    document.addEventListener("copy", listener);
+    document.execCommand("copy");
+    document.removeEventListener("copy", listener);
+
+    $("#alert-success").fadeIn();
 
     console.log("Signature Copied!")
 }
@@ -50,7 +67,11 @@ function formatPhoneNumber(phoneNumberString) {
     var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
     var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
     if (match) {
-      return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+        return '(' + match[1] + ') ' + match[2] + '-' + match[3]
     }
     return null
-  }
+}
+
+
+
+
