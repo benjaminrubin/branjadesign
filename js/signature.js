b92a213cc1408d35b20c9ccd22dce668b5156002
Benjamin Floyd Rubin <bennyfrubin@gmail.com>
2020-06-08 22:45:30 -0700

    Update

---
+++ js/signature.js
@@ -0,0 +1,47 @@
+// Form Variables
+var formName = $("#formName");
+var formEmail = $("#formEmail");
+var formPhone = $("#formPhone");
+var formRole = $("#formRole"); 
+
+// Signature Variables
+var signatureName = $("#signatureName");
+var signatureEmail = $("#signatureEmail");
+var signatureTarget = $("#signatureTarget");
+var signaturePhone = $("#signaturePhone");
+var signatureRole = $("#signatureRole"); 
+
+
+formName.keyup(function() {
+    signatureName.html(formName.val());
+});
+
+formEmail.keyup(function() {
+    
+    signatureEmail.html(formEmail.val());
+    var targetValue = "mailto:" + formEmail.val();
+    signatureTarget.attr('href', targetValue);
+});
+
+formPhone.keyup(function() {
+
+    var formattedPhone = formatPhoneNumber(formPhone.val());
+
+    signaturePhone.html(formattedPhone);
+})
+
+
+formRole.change(function() {
+    signatureRole.html(formRole.val())
+})
+
+
+
+function formatPhoneNumber(phoneNumberString) {
+    var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
+    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+    if (match) {
+      return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+    }
+    return null
+  }
